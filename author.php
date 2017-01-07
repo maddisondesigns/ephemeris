1c600b48572f960f5c310bcf45e1c6fccd7ee2ec
Anthony Hortin <anthony@maddisondesigns.com>
2017-01-07 18:12:21 +1100

    Use the_archive_title(). Change code to use proper braces rather than : endif/endwhile.

--- author.php
+++ author.php
@@ -13,40 +13,29 @@ get_header(); ?>
 
 		<div class="grid-70 tablet-grid-70">
 
-			<?php if ( have_posts() ) : ?>
-
-				<?php
-				// Queue the first post, that way we know what author we're dealing with (if that is the case).
-				// We reset this later so we can run the loop properly with a call to rewind_posts().
-				the_post();
-				?>
+			<?php if ( have_posts() ) { ?>
 
 				<header class="archive-header">
-					<h1 class="archive-title"><?php printf( esc_html__( 'Author Archives: %s', 'ephemeris' ), '<span class="vcard">' . get_the_author() . '</span>' ); ?></h1>
+					<?php the_archive_title(); ?>
+
+					<?php if ( get_the_author_meta( 'description' ) ) { // If a user has filled out their description, show a bio on their entries.
+						get_template_part( 'author-bio' );
+					} ?>
 				</header><!-- .archive-header -->
 
-				<?php // If a user has filled out their description, show a bio on their entries.
-				if ( get_the_author_meta( 'description' ) ) {
-					get_template_part( 'author-bio' );
-				} ?>
-
-				<?php
-				// Since we called the_post() above, we need to rewind the loop back to the beginning that way we can run the loop properly, in full.
-				rewind_posts();
-				?>
-
 				<?php // Start the Loop ?>
-				<?php while ( have_posts() ) : the_post(); ?>
+				<?php while ( have_posts() ) {
+					the_post(); ?>
 					<?php get_template_part( 'content', get_post_format() ); ?>
-				<?php endwhile; ?>
+				<?php } ?>
 
 				<?php ephemeris_posts_pagination(); ?>
 
-			<?php else : ?>
+			<?php } else { ?>
 
 				<?php get_template_part( 'no-results' ); // Include the template that displays a message that posts cannot be found ?>
 
-			<?php endif; // end have_posts() check ?>
+			<?php } // end have_posts() check ?>
 
 		</div> <!-- /.grid-70 -->
 		<?php get_sidebar(); ?>
