9d8021cd3bfb67e305c0448e24f24b58519d6916
Anthony Hortin <anthony@maddisondesigns.com>
2017-04-15 15:34:55 +1000

    Remove Autofocus from Search form

--- searchform.php
+++ searchform.php
@@ -1,7 +1,7 @@
 <form role="search" method="get" class="search-form" action="<?php echo get_home_url(); ?>">
 	<label>
 		<span class="screen-reader-text">Search for:</span>
-		<input type="search" class="search-field" placeholder="Search &hellip;" value="" name="s" autofocus="autofocus" />
+		<input type="search" class="search-field" placeholder="Search &hellip;" value="" name="s"/>
 	</label>
 	<button type="submit" class="search-submit"><i class="fa fa-search"></i></button>
 <input type='hidden' name='lang' value='en' /></form>
