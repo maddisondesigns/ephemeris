f6dc1d3b24e1e984966ace07a1be3997fa4047e2
Anthony Hortin <anthony@maddisondesigns.com>
2017-05-30 20:18:41 +1000

    Change customizer preview for search icon to use generic menu class. Fix tabs/spaces in code

--- js/customizer-preview.js
+++ js/customizer-preview.js
@@ -1,9 +1,10 @@
 jQuery( document ).ready(function($) {
+	// Append the search icon list item to the main nav
    wp.customize('search_menu_icon', function(control) {
       control.bind(function( controlValue ) {
 			if( controlValue == true ) {
 				// If the switch is on, add the search icon
-				$('#menu-short').append('<li class="menu-item menu-item-search"><a href="#" class="nav-search"><i class="fa fa-search"></i></a></li>');
+				$('.nav-menu').append('<li class="menu-item menu-item-search"><a href="#" class="nav-search"><i class="fa fa-search"></i></a></li>');
 			}
 			else {
 				// If the switch is off, remove the search icon
@@ -12,9 +13,10 @@ jQuery( document ).ready(function($) {
       });
    });
 
+	// Change the font-size of the h1
 	wp.customize('sample_slider_control', function(control) {
-      control.bind(function( controlValue ) {
+		control.bind(function( controlValue ) {
 			$('h1').css('font-size', controlValue + 'px');
-      });
-   });
+		});
+	});
 });
