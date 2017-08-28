621964d68603e56f00fc048b7d47e17a17064ec2
Anthony Hortin <anthony@maddisondesigns.com>
2017-08-28 13:21:33 +1000

    Update the Customizer Preview url when the WooCommerce panel is selected

---
+++ js/customize-controls.js
@@ -0,0 +1,12 @@
+(function ( api ) {
+	// When the WooCommerce panel is expanded, change preview url to WooCommerce shop page
+	api.section( 'woocommerce_layout_section', function( section ) {
+		section.expanded.bind( function( isExpanded ) {
+			var url;
+			if ( isExpanded ) {
+				url = ephemeris_woocommerce_data.ephemeris_woocommerce_url;
+				api.previewer.previewUrl.set( url );
+			}
+		} );
+	} );
+} ( wp.customize ) );
