8f14c64986de5708c23abbcfadd6aa561e159f8a
Anthony Hortin <anthony@maddisondesigns.com>
2017-09-27 01:13:53 +1000

    Display comments template on Left Sidebar template

--- template-left-sidebar.php
+++ template-left-sidebar.php
@@ -25,6 +25,12 @@ get_header(); ?>
 				while ( have_posts() ) {
 					the_post();
 					get_template_part( 'template-parts/content', 'page' );
+
+					// If comments are open or we have at least one comment, load up the comment template
+					if ( comments_open() || '0' != get_comments_number() ) {
+						comments_template( '', true );
+					}
+
 				} // end of the loop
 
 				ephemeris_posts_pagination();
