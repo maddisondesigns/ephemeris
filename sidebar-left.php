07e540f3e1f5ef79d410e4f970bebf6dce0254c0
Anthony Hortin <anthony@maddisondesigns.com>
2017-10-03 03:05:09 +1100

    Add new template for left-hand sidebar

---
+++ sidebar-left.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * The Left Hand Sidebar containing the main widget areas.
+ *
+ * @package Ephemeris
+ * @since Ephemeris 1.0
+ */
+?>
+<div <?php ephemeris_sidebar_class( true, 'pull-75 tablet-pull-75' ); ?>>
+
+	<?php get_template_part( 'template-parts/content', 'sidebar' ); ?>
+
+</div>
