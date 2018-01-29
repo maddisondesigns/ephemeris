ce07ece29b53caf0d6b6c855410754ae7599eade
Anthony Hortin <anthony@maddisondesigns.com>
2018-01-29 15:00:20 +1100

    Add missing singular placeholder to comments template. Remove usage of custom comment walker

--- comments.php
+++ comments.php
@@ -2,10 +2,7 @@
 /**
  * The template for displaying Comments.
  *
- * The area of the page that contains both current comments
- * and the comment form. The actual display of comments is
- * handled by a callback to ephemeris_comment() which is
- * located in the functions.php file.
+ * The area of the page that contains both current comments and the comment form.
  *
  * @package Ephemeris
  * @since Ephemeris 1.0
@@ -27,13 +24,36 @@ if ( post_password_required() )
 	<?php if ( have_comments() ) { ?>
 		<h2 class="comments-title">
 			<?php
-			printf( _n( 'One response on &ldquo;%2$s&rdquo;', '%1$s responses on &ldquo;%2$s&rdquo;', get_comments_number(), 'ephemeris' ),
-			number_format_i18n( get_comments_number() ), '<span>' . get_the_title() . '</span>' );
+			$comments_number = get_comments_number();
+			if ( '1' === $comments_number ) {
+				/* translators: %s: post title */
+				printf( _x( 'One Reply to &ldquo;%s&rdquo;', 'comments title', 'ephemeris' ), get_the_title() );
+			} else {
+				/* translators: 1: number of comments, 2: post title */
+				printf(
+					_nx(
+						'%1$s Reply to &ldquo;%2$s&rdquo;',
+						'%1$s Replies to &ldquo;%2$s&rdquo;',
+						$comments_number,
+						'comments title',
+						'ephemeris'
+					),
+					number_format_i18n( $comments_number ),
+					get_the_title()
+				);
+			}
 			?>
 		</h2>
 
 		<ol class="commentlist">
-			<?php wp_list_comments( array( 'callback' => 'ephemeris_comment', 'style' => 'ol' ) ); ?>
+			<?php
+				wp_list_comments( array(
+					'avatar_size' => 100,
+					'style'       => 'ol',
+					'short_ping'  => true,
+					'reply_text'  => '<i class="fa fa-reply" aria-hidden="true"></i> ' . __( 'Reply', 'ephemeris' ),
+				) );
+			?>
 		</ol> <!-- /.commentlist -->
 
 		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) { // are there comments to navigate through ?>
