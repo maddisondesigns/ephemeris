cd6d0021b70200f986ed8b734c7acdaf8a406d97
Anthony Hortin <hello@maddisondesigns.com>
2019-05-16 13:23:56 +1000

    Remove the space between ‘Search’ and the ellipsis in the placeholder

--- searchform.php
+++ searchform.php
@@ -1,7 +1,7 @@
 <form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url() ); ?>">
 	<label>
 		<span class="screen-reader-text">Search for:</span>
-		<input type="search" class="search-field" placeholder="<?php esc_attr_e( 'Search &hellip;', 'ephemeris' ); ?>" value="<?php the_search_query(); ?>" name="s"/>
+		<input type="search" class="search-field" placeholder="<?php esc_attr_e( 'Search&hellip;', 'ephemeris' ); ?>" value="<?php the_search_query(); ?>" name="s"/>
 	</label>
 	<button type="submit" class="search-submit"><i class="fas fa-search"></i></button>
 <input type='hidden' name='lang' value='en' /></form>
